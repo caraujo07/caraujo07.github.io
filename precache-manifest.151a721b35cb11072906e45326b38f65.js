25c99eaff385a8a39cfe3a29f886bfc8640cd309
caraujo07 <caraujo@jc.com.br>
2019-02-11 15:22:05 -0300

    Deploying the project

---
+++ precache-manifest.151a721b35cb11072906e45326b38f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46adab469a3905a884ef",
+    "url": "/static/js/main.46adab46.chunk.js"
+  },
+  {
+    "revision": "1c53900a0d0de3b2dc60",
+    "url": "/static/js/1.1c53900a.chunk.js"
+  },
+  {
+    "revision": "46adab469a3905a884ef",
+    "url": "/static/css/main.ca5ee358.chunk.css"
+  },
+  {
+    "revision": "c2bd61a931960b0bae52d1ee28bb2b84",
+    "url": "/index.html"
+  }
+];
