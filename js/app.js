fc3d662a85aaba39fe23d344892cb795344fddce
caraujo07 <caraujo@jc.com.br>
2019-08-20 21:12:02 -0300

    3E

---
+++ js/app.js
@@ -0,0 +1,74 @@
+// Function to animate the button correctly
+const btnx = () => {
+  const btn = document.querySelector(".navbar-toggler .toggle-wrapper");
+  console.log(btn);
+  if(!btn.classList.contains('on')) {
+    btn.classList.add('on');
+  }else{
+    btn.classList.remove('on');
+  }
+}
+
+
+const activelink = () => {
+  const href = location.pathname.replace('/caraujo07.github.io/', '');
+  $('.nav-link[href="' + href +'"').addClass('active');
+}
+
+
+activelink();
+
+
+// Formulario
+
+$('.formphp').on('submit', function() {
+	var emailContato = "computacional.cesar@gmail.com"; // Escreva aqui o seu e-mail
+
+	var that = $(this),
+			url = that.attr('action'),
+			type = that.attr('method'),
+			data = {};
+	
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+				name = that.attr('name'),
+				value = that.val();
+				
+		data[name] = value;
+	});
+	
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+		
+			if( $('[name="leaveblank"]').val().length != 0 ) {
+				$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-erro');
+				});
+			} else {
+				$('.formphp').html("<div id='form-send'></div>");
+				$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-send');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp').html("<div id='form-erro'></div>");
+			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+			.hide()
+			.fadeIn(1500, function() {
+			$('#form-erro');  
+		});
+		}
+	});
+	
+	return false;
+});
+
