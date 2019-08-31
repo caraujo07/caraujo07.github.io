c4e6186704c60d5976ebbba4b472c1a5d748c611
caraujo07 <caraujo@jc.com.br>
2019-08-31 18:37:13 -0300

    projeto blog

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function slideToggle() {
   let slider = document.getElementById('navbarSupportedContent');
 
   if(slider.style.left == '0px') {
-    slider.style.left = '-500px';
+    slider.style.left = '-100px';
   }else {
     slider.style.left = '0px';
   }
