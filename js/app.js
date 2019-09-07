dff076b8ea1ff9a88336b64953d087a4665db3bb
caraujo07 <caraujo@jc.com.br>
2019-09-06 22:12:29 -0300

    atualizando

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function slideToggle() {
   let slider = document.getElementById('navbarSupportedContent');
 
   if(slider.style.left == '0px') {
-    slider.style.left = '-1000px';
+    slider.style.left = '-1100px';
   }else {
     slider.style.left = '0px';
   }
