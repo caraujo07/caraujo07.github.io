c93d14fe5f83e879702a4b9d58b30157483ad6a8
caraujo07 <caraujo@jc.com.br>
2019-09-14 11:42:00 -0300

    williames

---
+++ js/app.js
@@ -0,0 +1,27 @@
+function slideToggle() {
+  let slider = document.getElementById('navbarSupportedContent');
+
+  if(slider.style.left == '0px') {
+    slider.style.left = '-1100px';
+  }else {
+    slider.style.left = '0px';
+  }
+}   
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 2,
+    nav: true,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      768: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  });
+});
